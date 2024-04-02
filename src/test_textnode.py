eecc413916c7719ebc8e435cfaca8083437e7d57
with-mridul <mridul.shrotriya@gmail.com>
2024-04-02 12:24:32 +0530

    worked on nodes

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -20,12 +20,12 @@ class TestTextNode(unittest.TestCase):
     def test_eq_false(self):
         node = TextNode("This is a text node", text_type_text)
         node2 = TextNode("This is a text node", text_type_bold)
-        self.assertEqual(node, node2)
+        self.assertNotEqual(node, node2)
 
     def test_eq_false2(self):
         node = TextNode("This is a text node", text_type_text)
         node2 = TextNode("This is a text node2", text_type_text)
-        self.assertEqual(node, node2)
+        self.assertNotEqual(node, node2)
 
     def test_eq_url(self):
         node = TextNode("This is a text node", text_type_italic, "https://www.boot.dev")
